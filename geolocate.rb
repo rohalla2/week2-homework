ea6472caf7b2a05524036b97babd82befdc6caf4
Jeff Rohalla <jrohalla@uchicago.edu>
2014-01-16 17:00:41 -0600

    adds functionality for zip code only

---
+++ geolocate.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+require 'json'
+
+puts "Enter an address, city or zipcode: "
+address = gets
+address = address.chomp
+
+json_data = open("http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=false").read
+
+data = JSON.parse(json_data)
+
+
+data = data["results"][0]["geometry"]["location"]
+
+puts "Latitude: #{data["lat"]}"
+puts "Longitude: #{data["lng"]}"
