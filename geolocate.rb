679a4f03f4c9060eecf447dff54f37931e91d9d9
Jeff Rohalla <jrohalla@uchicago.edu>
2014-01-17 13:52:22 -0600

    Adds comments to geolocate.rb

--- geolocate.rb
+++ geolocate.rb
@@ -1,18 +1,19 @@
 require 'open-uri'
 require 'json'
 
+#Accept user input
 puts "Enter an address, city or zipcode: "
 address = gets
-address = address.chomp
 
+#Clean user input
+address = address.chomp
 address.tr!(" ", "+")
 
+#make API request and get json data
 json_data = open("http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=false").read
-
 data = JSON.parse(json_data)
 
-
+#parse out desired info to return to user
 data = data["results"][0]["geometry"]["location"]
-
 puts "Latitude: #{data["lat"]}"
 puts "Longitude: #{data["lng"]}"
