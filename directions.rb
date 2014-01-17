5cc3346d3b3afdb306b89c8752bee902d195b317
Jeff Rohalla <jrohalla@uchicago.edu>
2014-01-17 13:59:15 -0600

    adds comments for directions.rb

--- directions.rb
+++ directions.rb
@@ -1,22 +1,27 @@
 require 'open-uri'
 require 'json'
 
+#accept user input
 puts "Enter an origin address: "
 origin = gets
+
+#clean user input
 origin = origin.chomp
 origin.tr!(" ", "+")
 
+#accept user input
 puts "Enter a destination address: "
 dest = gets
+
+#clean user input
 dest = dest.chomp
 dest.tr!(" ", "+")
 
-#maps.googleapis.com/maps/api/directions/json?origin=###&destination=###&sensor-false
-
+#fetch api data
 json_data = open("http://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{dest}&sensor=false").read
-
 data = JSON.parse(json_data)
 
+#display desired data points
 puts "Total travel time driving: #{data["routes"][0]["legs"][0]["duration"]["text"]}"
 puts "Total distance traveled: #{data["routes"][0]["legs"][0]["distance"]["text"]}"
 
